2a4800dfd589365c74a49477a93a2909cb802e5d
dfilter <dirk_mail@protonmail.com>
2021-11-22 16:44:37 -0500

    Type Guards

--- section-6-advanced-types/src/app.ts
+++ section-6-advanced-types/src/app.ts
@@ -23,3 +23,60 @@ type Numeric = number | boolean;
 
 // the only types that intersect are "number" therefor the only applicable type to Universal will be number
 type Universal = Combinable & Numeric;
+
+// type guarding:
+
+function add(a: Combinable, b: Combinable) {
+    // this is an example of a type guard:
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ', emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ', emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Privileges: ', emp.startDate);
+    }
+}
+
+printEmployeeInformation({name: 'Jane', startDate: new Date()});
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo... ', amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    // here we type guard in order to only run the loadCargo method if the passed object is an instance of Truck
+    // note that instanceof can only be used with classes, interfaces have no constructor so they won't have access to this.
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
