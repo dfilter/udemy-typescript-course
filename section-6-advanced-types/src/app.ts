3b1e52774ed61d8348a24344e8662a400f3f75f1
dfilter <dirk_mail@protonmail.com>
2021-11-22 16:52:12 -0500

    Discriminating unions

--- section-6-advanced-types/src/app.ts
+++ section-6-advanced-types/src/app.ts
@@ -80,3 +80,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated Unions
+// in order to discriminate between the interfaces we add a common property that describes it.
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving with speed: ', speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10});
